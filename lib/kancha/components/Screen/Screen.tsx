c6defa845d0c323eaa34c92dd9ae97fc689a1681
Jason Healy <hello@jasonhealy.io>
2019-01-18 21:06:38 +0000

    Adding more settings screens

--- lib/kancha/components/Screen/Screen.tsx
+++ lib/kancha/components/Screen/Screen.tsx
@@ -1,20 +1,47 @@
-import * as React from 'react';
+/***
+ *  Copyright (C) 2018 ConsenSys AG
+ *
+ *  This file is part of uPort Mobile App
+ *  uPort Mobile App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  uPort Mobile App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with uPort Mobile App.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ***/
+
+import * as React from 'react'
 import { SafeAreaView, ScrollView } from 'react-native'
-import { Container, Theme } from '@kancha';
+import { Container, Theme } from '@kancha'
 
 interface ScreenProps {
-  safeAreaView?: boolean;
-  type?: 'primary' | 'secondary' | undefined;
+  safeAreaView?: boolean
+  type?: 'primary' | 'secondary' | undefined
 }
 
-const Screen: React.FunctionComponent<ScreenProps> = (props) => {
+const Screen: React.FunctionComponent<ScreenProps> = props => {
   return props.safeAreaView ? (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView style={{ backgroundColor: props.type && Theme.colors[props.type].background }}>
-        <Container flex={1} background={props.type}>{props.children}</Container>
+      <ScrollView
+        style={{
+          backgroundColor: props.type && Theme.colors[props.type].background,
+        }}
+      >
+        <Container flex={1} background={props.type} paddingBottom>
+          {props.children}
+        </Container>
       </ScrollView>
     </SafeAreaView>
-  ) : <Container flex={1}>{props.children}</Container>
+  ) : (
+    <Container flex={1}>{props.children}</Container>
+  )
 }
 
 Screen.defaultProps = {
@@ -22,4 +49,4 @@ Screen.defaultProps = {
   type: 'secondary',
 }
 
-export default Screen;
+export default Screen
--- lib/kancha/components/Text/Text.tsx
+++ lib/kancha/components/Text/Text.tsx
@@ -1,35 +1,52 @@
-import * as React from 'react';
-import { Text, TextStyle } from 'react-native';
-import { TextThemeMap, TextTypes } from '@kancha';
+/***
+ *  Copyright (C) 2018 ConsenSys AG
+ *
+ *  This file is part of uPort Mobile App
+ *  uPort Mobile App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
 
- /**
-  * Kancha Text Props
-  */
+ *  uPort Mobile App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  ERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with uPort Mobile App.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ***/
+
+import * as React from 'react'
+import { Text, TextStyle } from 'react-native'
+import { TextThemeMap, TextTypes } from '@kancha'
+
+/**
+ * Kancha Text Props
+ */
 interface KanchaTextProps {
-
   /**
    * The type of text to display. This will be styled accordinly to the theme
    */
-  type: string;
+  type: string
 
   /**
    * Make the text bold
    */
-  bold?: boolean;
+  bold?: boolean
 
   /**
    * The padding around the text
    */
-  padding?: number;
+  padding?: number
 
   /**
    * The margin around the text
    */
-  margin?: number;
+  margin?: number
 }
 
-const KanchaText: React.FC<KanchaTextProps> = (props) => {
-
+const KanchaText: React.FC<KanchaTextProps> = props => {
   const styles: TextStyle = {
     ...TextThemeMap[props.type],
     fontWeight: props.bold && 'bold',
@@ -38,4 +55,4 @@ const KanchaText: React.FC<KanchaTextProps> = (props) => {
   return <Text style={styles}>{props.children}</Text>
 }
 
-export default KanchaText;
+export default KanchaText
