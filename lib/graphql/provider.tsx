93c070dda413a6126c2b13b84ec81802c66da0b6
Simonas Karuzas <simonas@not.cat>
2019-02-25 14:23:09 +0200

    Support for external GQL endpoint

--- lib/graphql/provider.tsx
+++ lib/graphql/provider.tsx
@@ -3,7 +3,7 @@ import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { SchemaLink } from 'apollo-link-schema'
-
+import { createHttpLink } from 'apollo-link-http'
 import { Provider } from 'react-redux'
 import { Api, typeDefs, resolvers } from 'uport-graph'
 import { driver } from './db-rn-sqlite3'
@@ -16,12 +16,22 @@ export const schema = makeExecutableSchema({
 
 const api = new Api(driver)
 
+let link
+
+// local
+link = new SchemaLink({
+  schema,
+  context: { api },
+})
+
+// external
+// link = createHttpLink({
+//   uri: 'https://c685bd67.ngrok.io/graphql',
+// })
+
 const graphqlClient = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new SchemaLink({
-    schema,
-    context: { api },
-   }),
+  link,
 })
 
 export default (props: any) => (
