1d75aca5e1e1bcf6b4f6c2bd8e86f87ee99b9395
Simonas Karuzas <simonas@not.cat>
2019-02-28 20:47:07 +0200

    Using component from uport-components

--- lib/graphql/provider.tsx
+++ lib/graphql/provider.tsx
@@ -1,3 +1,20 @@
+// Copyright (C) 2018 ConsenSys AG
+//
+// This file is part of uPort Mobile App.
+//
+// uPort Mobile App is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// uPort Mobile App is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with uPort Mobile App.  If not, see <http://www.gnu.org/licenses/>.
+//
 import React from 'react'
 import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
@@ -26,7 +43,7 @@ link = new SchemaLink({
 
 // external
 // link = createHttpLink({
-//   uri: 'https://c685bd67.ngrok.io/graphql',
+//   uri: 'https://bce992e3.ngrok.io/graphql',
 // })
 
 const graphqlClient = new ApolloClient({
