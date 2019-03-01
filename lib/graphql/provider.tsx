b3952ee251d8d70bc9991258db33916ba196dea6
Simonas Karuzas <simonas@not.cat>
2019-03-01 16:02:40 +0200

    Components from uport-graph mono repo

--- lib/graphql/provider.tsx
+++ lib/graphql/provider.tsx
@@ -22,7 +22,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { SchemaLink } from 'apollo-link-schema'
 import { createHttpLink } from 'apollo-link-http'
 import { Provider } from 'react-redux'
-import { Api, typeDefs, resolvers } from 'uport-graph'
+import { Api, typeDefs, resolvers } from 'uport-graph-api'
 import { driver } from './db-rn-sqlite3'
 import { makeExecutableSchema } from 'graphql-tools'
 
