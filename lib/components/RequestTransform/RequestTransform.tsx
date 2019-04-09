4952f67d71bf84f5e1da24a7b2677156545f256d
Jason Healy <hello@jasonhealy.io>
2019-04-09 17:30:11 +0100

    Add connect

--- lib/components/RequestTransform/RequestTransform.tsx
+++ lib/components/RequestTransform/RequestTransform.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
 
+/**
+ * The only request related component that needs to be connected to Redux
+ * This HOC should take data from redux and format it in the typed Request shape
+ */
 interface RequestTransformProps {}
 
 const RequestTransform = <P extends object>(RequestComponent: React.ComponentType<P>) => {
