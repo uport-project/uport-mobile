2a0b5ab99d5c93d10b656f8fb3980e13676ba59e
Jason Healy <hello@jasonhealy.io>
2019-06-13 10:25:10 +0100

    Upgrade React Native to 0.59 (#146)
    * Upgrade working IOS
    
    * Upgrade working on Android
    
    * Add postinstall script
    
    * Update metro configs
    
    * Bumped to 0.59.8
    
    * update react-native-fetch-blob to rn-fetch-blob
    
    * bump snapshots
    
    * fix some complaints
    
    * use @react-native-community/async-storage
    
    * move enyme-to-json to dev
    
    * Use local updated mori
    
    * use updated mori
    
    * Finsh AsyncStorage installation android
    
    * Replace queue with regular js
    
    * increase memory when building ios prod
    
    * use external netinfo
    
    * fix failures in transactionRequest
    
    * Install netinfo android
    
    * Fix snapshots after rebase

---
+++ metro.config.js
@@ -0,0 +1,29 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ */
+
+module.exports = {
+  transformer: {
+    minifierPath: 'metro-minify-terser',
+    // minifierConfig: {
+    //   // ecma: 8,
+    //   // keep_classnames: true,
+    //   // keep_fnames: true,
+    //   // module: true,
+    //   mangle: {
+    //     // module: true,
+    //     // keep_classnames: true,
+    //     // keep_fnames: true,
+    //   },
+    // },
+    getTransformOptions: async () => ({
+      transform: {
+        babelTransformerPath: require.resolve('react-native-typescript-transformer'),
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+}
--- scripts/clean_and_bundle.sh
+++ scripts/clean_and_bundle.sh
@@ -5,4 +5,4 @@ bash scripts/clean_script.sh
 echo "Bundling..."
 
 
-react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
