346db63de29629ec8df0c4a2532820ffe2b74aaf
Jason Healy <hello@jasonhealy.io>
2019-06-19 12:20:00 +0100

    Add webview to pull in privacy policy

--- lib/screens/onboarding/Privacy.tsx
+++ lib/screens/onboarding/Privacy.tsx
@@ -1,47 +1,27 @@
 import * as React from 'react'
-import { Navigation } from 'react-native-navigation'
-import { Screen, Container, Text, NavBar } from '@kancha'
-
-import termsConditions from 'uPortMobile/lib/content/privacyContent.json'
+import { WebView } from 'react-native'
+import { Screen } from '@kancha'
 
 interface PrivacyProps extends Kancha.NavigationScreen {}
 
-const Privacy: React.FC<PrivacyProps> = ({ componentId }) => {
+const overideStyle = `(function() {
+  document.querySelector('.w-nav').style.display = 'none'
+  document.querySelector('#home').style.marginTop = '-100px'
+  document.querySelector('#home').style.height = '200px'
+  document.querySelector('#about').style.paddingTop = '20px'
+  document.querySelector('.footer-section').style.display = 'none'
+  document.querySelector('.hide-mobile-app').style.display = 'none'
+})()`
+
+const Privacy: React.FC<PrivacyProps> = () => {
   return (
-    <Screen statusBarHidden type={Screen.Types.Primary} config={Screen.Config.Scroll}>
-      <Container padding>
-        <Text type={Text.Types.H2} bold>
-          Privacy Policy
-        </Text>
-        <Text type={Text.Types.SubTitle}>{termsConditions.version}</Text>
-        <Container paddingTop paddingBottom>
-          <Text>{termsConditions.notice}</Text>
-        </Container>
-        <Container paddingTop paddingBottom>
-          <Text>{termsConditions.summary}</Text>
-        </Container>
-        {termsConditions.sections.map(section => {
-          return (
-            <Container key={section.section}>
-              <Container>
-                <Text bold type={Text.Types.H3}>
-                  {section.title}
-                </Text>
-              </Container>
-              {section.clauses.map(clause => {
-                return (
-                  <Container paddingBottom key={clause.number}>
-                    <Text bold>
-                      {section.clauses.length > 1 && clause.number} {clause.title && clause.title}
-                    </Text>
-                    <Text>{clause.content}</Text>
-                  </Container>
-                )
-              })}
-            </Container>
-          )
-        })}
-      </Container>
+    <Screen statusBarHidden type={Screen.Types.Primary} config={Screen.Config.NoScroll}>
+      <WebView
+        source={{ uri: 'https://www.uport.me/privacy-policy' }}
+        injectedJavaScript={overideStyle}
+        scalesPageToFit={false}
+        startInLoadingState={true}
+      />
     </Screen>
   )
 }
