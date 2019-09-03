4c7adf3e87e8d08b4f553a30b0e8420a3cc84a02
Aldi Gjoka <aldigjo@gmail.com>
2019-09-03 16:16:59 -0400

    Pass in component ID to markeyplace modal

--- lib/screens/MarketPlace.tsx
+++ lib/screens/MarketPlace.tsx
@@ -2,13 +2,14 @@ import * as React from 'react'
 import { Container, MarketPlace } from '@kancha'
 
 interface MarketPlaceModalProps {
+  componentId: string,
   config: any
 }
 
 const MarketPlaceModal: React.FC<MarketPlaceModalProps> = props => {
   return (
     <Container flex={1} justifyContent={'flex-end'} padding>
-      <MarketPlace config={props.config} />
+      <MarketPlace config={props.config} componentId={props.componentId}/>
     </Container>
   )
 }
